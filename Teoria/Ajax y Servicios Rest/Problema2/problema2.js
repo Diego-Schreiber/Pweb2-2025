a02f7d13ea6a1b3748fce1e20095fc304402f781
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-04-22 22:23:56 -0500

    Funcion que carga datos de json

--- Teoria/Ajax y Servicios Rest/Problema2/problema2.js
+++ Teoria/Ajax y Servicios Rest/Problema2/problema2.js
@@ -4,3 +4,15 @@
     const b = Math.floor(Math.random() * 255);
     return 'rgb(' + r + ',' + g + ',' + b + ')';
   }
+
+  async function cargarDatos() {
+   try {
+    const respuesta = await fetch('data.json');
+    const datos = await respuesta.json();
+    return datos;
+  } catch (error) {
+    console.error('Error al cargar el archivo JSON:', error);
+    return [];
+  }
+}
+
