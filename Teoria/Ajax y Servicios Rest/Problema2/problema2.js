651d11be64dad8e5bcdb201c7eaabb7cf70088f0
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-04-22 22:16:32 -0500

    funcion color aleatorio

---
+++ Teoria/Ajax y Servicios Rest/Problema2/problema2.js
@@ -0,0 +1,6 @@
+  function generarColorAleatorio() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
