4563861deb059366d34df71d383e0336895f4f66
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-13 18:30:40 -0500

    funcion join

--- Laboratorio/Laboratorio05/picture.py
+++ Laboratorio/Laboratorio05/picture.py
@@ -27,9 +27,8 @@ class Picture:
     return Picture(nueva_img)
 
   def join(self, p):
-    """ Devuelve una nueva figura poniendo la figura del argumento 
-        al lado derecho de la figura actual """
-    return Picture(None)
+    nueva_img = [linea1 + linea2 for linea1, linea2 in zip(self.img, p.img)]
+    return Picture(nueva_img)
 
   def up(self, p):
     return Picture(None)
