b70ec845a947e87609c9fa64061228eb9485e78f
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-24 16:00:33 -0500

    Modelos en ventas

--- Laboratorio/Laboratorio06/sistema_ventas/ventas/models.py
+++ Laboratorio/Laboratorio06/sistema_ventas/ventas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from productos.models import Producto
 
-# Create your models here.
+class Venta(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
