961c37ef27818af3d9e0fa9d79c7b68503b715c3
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-29 23:27:10 -0500

    Modificando views

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def myHomeView(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+def anotherView(request):
+    return HttpResponse('<h1>Sólo otra página</h1>').
