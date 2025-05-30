0976e9b58f7045f73e3425bf49a813255b05d364
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-05-29 23:21:49 -0500

    Migracion

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    donador = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
