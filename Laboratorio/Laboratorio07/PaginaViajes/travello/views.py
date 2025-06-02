7c71ff2c583582ae66a359308bf353f63fc2dcd0
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-02 15:19:15 -0500

    MOdificando special offer con if

--- Laboratorio/Laboratorio07/PaginaViajes/travello/views.py
+++ Laboratorio/Laboratorio07/PaginaViajes/travello/views.py
@@ -11,16 +11,19 @@ def index(request):
     dest1.desc = 'The city that never sleeps'
     dest1.img = 'destination_1.jpg'
     dest1.price = 700
+    dest1.offer = False
 
     dest2.name ='Indonesia'
     dest2.desc = 'Ni idea de indonesia'
     dest2.img= 'destination_2.jpg'
     dest2.price = 679
+    dest2.offer = True
 
     dest3.name = 'San Francisco'
     dest3.desc = 'Aqui hay un puente creo'
     dest3.img = 'destination_3.jpg'
     dest3.price = 679
+    dest3.offer = False
 
     dests= [dest1,dest2,dest3]
  
