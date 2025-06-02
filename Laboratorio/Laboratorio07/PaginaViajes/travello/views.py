f7d5db6729374745feaf29368950be0e296230e0
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-02 02:03:48 -0500

    informacion dinamica

--- Laboratorio/Laboratorio07/PaginaViajes/travello/views.py
+++ Laboratorio/Laboratorio07/PaginaViajes/travello/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from .models import Destination
 
 def index(request):
-    return render(request, 'travello/index.html')
+
+    dest1= Destination()
+    dest2= Destination()
+    dest3= Destination()
+
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The city that never sleeps'
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 700
+
+    dest2.name ='Indonesia'
+    dest2.desc = 'Ni idea de indonesia'
+    dest2.img= 'destination_2.jpg'
+    dest2.price = 679
+
+    dest3.name = 'San Francisco'
+    dest3.desc = 'Aqui hay un puente creo'
+    dest3.img = 'destination_3.jpg'
+    dest3.price = 679
+
+    dests= [dest1,dest2,dest3]
+ 
+    return render(request, 'travello/index.html', {'dests': dests})
