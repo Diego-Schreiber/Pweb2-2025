10717167973d75885a7f5585bf10c656d4c85413
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-02 19:46:43 -0500

    register funcionando

--- Laboratorio/Laboratorio07/PaginaViajes/account/views.py
+++ Laboratorio/Laboratorio07/PaginaViajes/account/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
+def login(request):
+    pass
+
 def register(request):
 
     if request.method == 'POST':
@@ -13,17 +16,19 @@ def register(request):
         password2 = request.POST['password2']
    
         if password1==password2:
-            if User.objects.filter(username=usaername).exists():
+            if User.objects.filter(username=username).exists():
                 messages.info(request,'Usuario repetido')
-            elif: User.objects.filter(email=email).exists():
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
                 messages.info(request, 'email repetido')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username,password=password1, email=email, first_name=first_name, last_name=last_name)    
                 user.save();
         else: 
            messages.info(request,'different passwords')
+           return redirect('register')
         return redirect('/')
 
     else:
-
-    return render(request, ''register.html')
+        return render(request, 'account/register.html')
