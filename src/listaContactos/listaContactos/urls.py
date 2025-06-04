cf44ec3d3888a28a049f550eb42fc9493e6317b7
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-04 17:31:51 -0500

    Añadiendo los urls a los path

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from inicio.views import myHomeView, anotherView
 from personas.views import listar_personas
+from personas import views
 
 urlpatterns = [
     path('', myHomeView, name='pagina_inicio'),
     path('another/', anotherView, name='otra_pagina'),
     path('personas/', listar_personas, name='lista_personas'),
     path('admin/', admin.site.urls),
+    path('personas/nueva/', views.persona_create, name='persona_create'),
 ]
