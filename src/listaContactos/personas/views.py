a2cf4be8ffc3940db9f7a940ff43ce9dfc77e967
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-04 17:29:28 -0500

    Añadiendo persona_create en views.py

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 
 def listar_personas(request):
     personas = Persona.objects.all()
@@ -8,3 +9,10 @@ def listar_personas(request):
 def mi_vista(request):
     datos = {'nombre': 'Ana', 'edad': 25}
     return render(request, 'mi_template.html', datos)
+
+def persona_create(request):
+    form = PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = PersonaForm() 
+    return render(request, 'personas/personasCreate.html', {'form': form})
