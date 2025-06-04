e0484d8886593584265b87b1bf69e9cd51ecf22c
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-04 17:19:56 -0500

    Añadiendo vista a views.py(1er commit)

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -4,3 +4,7 @@ from .models import Persona
 def listar_personas(request):
     personas = Persona.objects.all()
     return render(request, "personas.html", {'personas': personas})
+
+def mi_vista(request):
+    datos = {'nombre': 'Ana', 'edad': 25}
+    return render(request, 'mi_template.html', datos)
