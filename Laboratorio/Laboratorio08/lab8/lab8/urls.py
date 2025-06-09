e58aedd5e8a1d042a8429306933115d25869a629
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-08 23:42:37 -0500

    Enviar gmail

--- Laboratorio/Laboratorio08/lab8/lab8/urls.py
+++ Laboratorio/Laboratorio08/lab8/lab8/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import pdf_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pdf/', pdf_view),
     path('',include('example.urls')),
 ]
--- Laboratorio/Laboratorio08/lab8/lab8/views.py
+++ Laboratorio/Laboratorio08/lab8/lab8/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 def index(request):
-    send_mail('Hola de Diego', 'Este es un mensaje de prueba','dschreiber@unsa.edu.pe',['diegoschlan@gmail.com'], fail_silently=False)
+    send_mail('Hola de Diego', 'Este es un mensaje de prueba','dschreiber@unsa.edu.pe',('diegoschlan@gmail.com'), fail_silently=False)
     return render(request, 'example/index.html')
 
