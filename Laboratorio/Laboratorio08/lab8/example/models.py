da8b65d921acd20ede14b7f0393299e4a2500bf6
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-06-08 22:01:03 -0500

    Modelos personajes y peliculas

--- Laboratorio/Laboratorio08/lab8/example/models.py
+++ Laboratorio/Laboratorio08/lab8/example/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 class Language(models.Model):
     name = models.CharField(max_length=10)
-    def _str_(self):
+    def __str__(self):
         return self.name 
 
 class Framework(models.Model):
     name = models.CharField(max_length=10)
     language = models.ForeignKey(Language, on_delete=models.CASCADE) 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
