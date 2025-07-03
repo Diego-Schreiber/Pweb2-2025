ccc842535e990f2ea79566e15f29428a82b2c78d
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-03 14:51:14 -0500

    Building dist

--- Teoria/Angular/proyecto-angular/src/main.ts
+++ Teoria/Angular/proyecto-angular/src/main.ts
@@ -1,22 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { AppComponent } from './app/app';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(FormsModule, HttpClientModule)
-  ]
-});
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-});
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
