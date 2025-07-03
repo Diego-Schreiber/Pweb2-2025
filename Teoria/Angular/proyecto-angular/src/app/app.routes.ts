2b72ba14dd772398db4cf7edc7ac1acca48df4fc
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-03 13:33:05 -0500

    Añadiendo rutas de componenetes

--- Teoria/Angular/proyecto-angular/src/app/app.routes.ts
+++ Teoria/Angular/proyecto-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HelloComponent } from './hello/hello.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'hello', component: HelloComponent },
+  { path: '', redirectTo: 'hello', pathMatch: 'full' }
+];
+
