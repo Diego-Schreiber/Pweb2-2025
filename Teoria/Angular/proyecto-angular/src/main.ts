a4311c2177ddaf00cf862046feed926fcfc39f8d
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-03 13:36:33 -0500

    Añadiendo import y provider para formulario

--- Teoria/Angular/proyecto-angular/src/main.ts
+++ Teoria/Angular/proyecto-angular/src/main.ts
@@ -4,7 +4,15 @@ import { App } from './app/app';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(FormsModule)
+  ]
+});
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
 });
