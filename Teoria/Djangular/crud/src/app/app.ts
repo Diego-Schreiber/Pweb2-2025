a91bea455042ee459966fb569ce951d03600e1f7
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-10 13:23:11 -0500

    Añadiendo logica de la app

--- Teoria/Djangular/crud/src/app/app.ts
+++ Teoria/Djangular/crud/src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Api } from './api';  // nombre real del servicio
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'crud';
+export class AppComponent {
+  movies: any[] = [];
+  api = inject(Api);
+
+  constructor() {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.api.getAllMovies().subscribe(
+      data => this.movies = data,
+      error => console.error(error)
+    );
+  }
 }
+
